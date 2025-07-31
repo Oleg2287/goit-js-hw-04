f1728e6e98d1c5dba04a66c89db561f355b2f7b4
Oleg <luchkoo22@gmail.com>
2025-07-31 10:18:59 +0300

    task1

--- js/task-1.js
+++ js/task-1.js
@@ -17,7 +17,6 @@ function isEnoughCapacity(products, containerSize) {
   return total <= containerSize;
 }
 
-// // Перевірка роботи функції
 console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
 
 console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
